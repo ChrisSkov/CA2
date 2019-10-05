eed542b17db691905254060de74fba9823b76346
Chris <Stofferdyr@gmail.com>
2019-10-06 00:07:53 +0200

    basic setup of entity classes and some relations

---
+++ src/main/java/entities/Person.java
@@ -0,0 +1,145 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author stoff
+ */
+@Entity
+@NamedQuery(name = "Person.deleteAllRows", query = "DELETE from Person")
+public class Person implements Serializable {
+//TODO ADD cascading to relations
+//TODO ADD proper constructor and more getters and setter
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String eMail;
+    private String firstName;
+    private String lastName;
+    
+    @ManyToOne
+    private Address address;
+    
+    @OneToMany
+    private Phone phone;
+
+    public String geteMail()
+    {
+        return eMail;
+    }
+
+    public void seteMail(String eMail)
+    {
+        this.eMail = eMail;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public Address getAddress()
+    {
+        return address;
+    }
+
+    public void setAddress(Address address)
+    {
+        this.address = address;
+    }
+
+    public Phone getPhone()
+    {
+        return phone;
+    }
+
+    public void setPhone(Phone phone)
+    {
+        this.phone = phone;
+    }
+
+    public Hobby getHobby()
+    {
+        return hobby;
+    }
+
+    public void setHobby(Hobby hobby)
+    {
+        this.hobby = hobby;
+    }
+
+    @ManyToMany
+    private Hobby hobby;
+
+    public Integer getId()
+    {
+        return id;
+    }
+
+    public void setId(Integer id)
+    {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object)
+    {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Person))
+        {
+            return false;
+        }
+        Person other = (Person) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id)))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "entities.Person[ id=" + id + " ]";
+    }
+    
+}
