6b664eb56f929a51d255065ee01ce5b6286a6e96
# Created by https://www.gitignore.io/api/windows,netbeans,java-web <cph-ss451@cphbusiness.dk>
2019-10-08 12:17:10 +0200

    a

--- src/main/java/facades/PersonFacade.java
+++ src/main/java/facades/PersonFacade.java
@@ -67,10 +67,48 @@ public class PersonFacade {
             return em.createQuery("SELECT p FROM Person p WHERE p.id = :id", Person.class).getSingleResult();
         } catch (Exception e) {
             e.printStackTrace();
-            throw new Exception("Non found by that name");
+            throw new Exception("Non found by that id");
         } finally {
             em.close();
         }
     }
+    
+    public Person getPersonByPhoneNumber(int phoneNumber) throws Exception {
+        EntityManager em = getEntityManager();
+        try {
+            return em.createQuery("SELECT p FROM Person p WHere p.phones = :phones", Person.class).getSingleResult();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new Exception("None found by that number");
+        } finally {
+            em.close();
+        }
+    }
+    
+    public Person getPersonByHobby(String name) throws Exception {
+        EntityManager em = getEntityManager();
+        try{
+            return em.createQuery("SELECT p FROM Person p JOIN p.hobbies h WHERE h.name = :name",Person.class).getSingleResult();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new Exception("None found with that hobby");
+        } finally {
+            em.close();
+        }
+    }
+    
+//    public Person getAllPersonFromCity(String cityName) throws Exception {
+//        EntityManager em = getEntityManager();
+//        try {
+//            return em.createQuery("SELECT count(p) FROM Person p JOIN p.address a WHERE a.cityInfo = :cityInfo");
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            throw new Exception("none from that city");
+//        }finally {
+//            em.close();
+//        }
+//    }
+//    
+    
 
 }
