dfe126d78539c5854e4ddf3265649d60e31a8f20
artin <artin-4000@hotmail.com>
2019-10-21 12:15:26 +0200

    Fetch hobby and zip added

--- CA2-front/src/index.js
+++ CA2-front/src/index.js
@@ -1,5 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.css'
 
+window.onload = (event) => {
+    var url = "https://goinghamdev.com/CA2/api/allPersonsHobby/Hobby";
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            document.getElementById("getHobby").innerHTML = toSelect(data);
+        })
+
+        var url2 = "https://goinghamdev.com/CA2/api/allZips";
+    fetch(url2)
+        .then(res => res.json())
+        .then(data => {
+            document.getElementById("getZip").innerHTML = toSelectZip(data);
+        })
+};
 
 
 
